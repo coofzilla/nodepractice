81559741b8f0b7cbd8541a22bf64838b59aea09f
Jay Hernandez <jeric614@yahoo.com>
2021-09-14 12:13:44 +0900

    cached users posts w/nested hashes

--- services/cache.js
+++ services/cache.js
@@ -4,11 +4,13 @@ const { promisify } = require("util");
 
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
-client.get = promisify(client.get);
+client.hget = promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+
   return this;
 };
 
@@ -24,7 +26,7 @@ mongoose.Query.prototype.exec = async function () {
   );
 
   //see if have a value for key in redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   //if do, return
   if (cacheValue) {
@@ -36,8 +38,8 @@ mongoose.Query.prototype.exec = async function () {
   }
   //otherwise, issue query and store the result in redis
   const result = await exec.apply(this, arguments);
-
-  client.set(key, JSON.stringify(result));
+  //for setting expiration of cache
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
 
   return result;
 };

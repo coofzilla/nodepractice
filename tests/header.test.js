bbcd1d7dcd93371daad0caed8f10205253826d74
Jay Hernandez <jeric614@yahoo.com>
2021-09-19 15:06:33 +0900

    added test f/oauth url

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,16 +1,31 @@
 //jest tests need to end in .test.js
 const puppeteer = require("puppeteer");
+let browser, page;
 
-//first argument describes the test, second is the logic to test
-test("Adds two numbers", () => {
-  const sum = 1 + 2;
-
-  expect(sum).toEqual(3);
-});
-//headless = open browser w/o gui
-test("We can launch a browser", async () => {
-  const browser = await puppeteer.launch({
+beforeEach(async () => {
+  browser = await puppeteer.launch({
     headless: false,
   });
-  const page = await browser.newPage();
+  page = await browser.newPage();
+  await page.goto("localhost:3000");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+//headless = open browser w/o gui
+test("header has correct text", async () => {
+  //$eval from puppeteer basically does queryselector
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+
+  expect(text).toEqual("Blogster");
+});
+
+test("clicking login starts oauth flow", async () => {
+  await page.click(".right a");
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
 });
